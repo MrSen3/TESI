7c59b1783c95ac81935d4b517d3a552a1c5baf8c
Silvio <s228789@polito.it>
2019-11-07 01:44:23 +0100

    Aggiunti istogramma con legenda interattiva, bubble plot per le riviste e modifiche generali

--- visualizza.js
+++ visualizza.js
@@ -4,6 +4,7 @@
  *
  * @param {String} url - URL della risorsa da caricare
  * @param {loadCallback} success - funzione di callback invocata con il contenuto
+ * @param {int} n - indica dove deve essere posizionato il grafico all'interno della pagina
  */
 //Uguale a quella scritta dal prof sulle slide
 function loadASync(url, success, n) {
@@ -38,60 +39,97 @@ function okStatus(s) {
 }
 
 //Flag che mi servono a stampare i grafici nell'ordine corretto
-var scatter=false;
-var histogram=false;
-var bubble=false;
-var wordcloud=false;
+//var scatter=false;
+//var histogram1=false;
+//var histogram2=true;
+//var bubble=false;
+//var wordcloud=false;
 
 function visualizza (data, n) {
     console.log(n+" "+data);
     let json = JSON.parse(data);
 
     if(n==1){
-        let base=document.getElementById("profilo");
-        let el=document.createElement("div");
-        el.setAttribute("id", "scatterplot");
-        base.appendChild(el);
-        vegaEmbed("#scatterplot", json);
-        scatter=true;
-        console.log(scatter);
+        //let base=document.getElementById("appoggio");
+        //let el=document.createElement("div");
+        //el.setAttribute("id", "wordcloud");
+        //base.appendChild(el);
+
+        //vegaEmbed("#scatterplot", json);
+        vegaEmbed("#scatterplot", json)
+            .then(result => console.log("ciao"))
+            .catch(console.warn);
+        //wordcloud=true;
     }
-    console.log(scatter);
-    if(n==2 && scatter==true){
-        console.log(scatter);
-        let base=document.getElementById("scatterplot");
-        let el=document.createElement("div");
-        el.setAttribute("id", "histogram");
-        base.appendChild(el);
+
+    if(n==2){
+    //if(n==2 && wordcloud==true){
+        //let base=document.getElementById("wordcloud");
+        //let el=document.createElement("div");
+        //el.setAttribute("id", "scatterplot");
+        //base.appendChild(el);
 
         //vegaEmbed('#histogram', json).then(res =>
           //  res.view
-            //    .insert('pubblicazioniDeRussisEsportate.json', [
+            //    .insert('pubblicazioni1.json', [
                     /* some data array */
               //  ])
                 //.run()
         //);
-        vegaEmbed("#histogram", json);
-        histogram=true;
+        //
+        //Se vegaEmbed ha esito positivo allora viene eseguito quello che c'e' dentro al then, altrimenti viene eseguito quello dentro al catch
+        vegaEmbed("#wordcloud", json)
+            .then(result => console.log("ciao"))
+            .catch(console.warn);
+        // scatterplot=true;
     }
-    if(n==3 && histogram==true){
-        let base=document.getElementById("histogram");
-        let el=document.createElement("div");
-        el.setAttribute("id", "bubbleplot");
-        base.appendChild(el);
-        vegaEmbed("#bubbleplot", json);
-        bubble=true;
-    }
-    if(n==4 && bubble==true){
-        let base=document.getElementById("bubbleplot");
-        let el=document.createElement("div");
-        el.setAttribute("id", "wordcloud");
-        base.appendChild(el);
-        vegaEmbed("#wordcloud", json);
-        wordcloud=true;
+    if(n==3){
+    //if(n==3 && scatterplot==true){
+    //     let base=document.getElementById("scatterplot");
+    //     let el=document.createElement("div");
+    //     el.setAttribute("id", "histogram1");
+    //     base.appendChild(el);
+        vegaEmbed("#histogram1", json)
+            .then(result => console.log("ciao"))
+            .catch(console.warn);
+        // histogram1=true;
     }
 
+    if(n==4){
+    //if(n==4 && histogram1==true){
+        //let base=document.getElementById("histogram1");
+        //let el=document.createElement("div");
+        //el.setAttribute("id", "histogram2");
+        //base.appendChild(el);
+        vegaEmbed("#bubbleplot1", json)
+            .then(result => console.log("ciao"))
+            .catch(console.warn);
+        //histogram2=true;
+    }
 
+    if(n==5){
+    //if(n==5 && histogram2==true){
+        //let base=document.getElementById("histogram2");
+        //let el=document.createElement("div");
+        //el.setAttribute("id", "bubbleplot");
+        //base.appendChild(el);
+        vegaEmbed("#histogram2", json)
+            .then(result => console.log("ciao"))
+            .catch(console.warn);
+        //bubble=true;
+    }
+
+    if (n==6){
+    //if(n==6 && histogram2==true){
+        //let base=document.getElementById("histogram2");
+        //let el=document.createElement("div");
+        //el.setAttribute("id", "bubbleplot");
+        //base.appendChild(el);
+        vegaEmbed("#bubbleplot2", json)
+            .then(result => console.log("ciao"))
+            .catch(console.warn);
+        //bubble=true;
+    }
     //se il json ha l'indicazione 'update' allora creo un paragrafo <p>, ci scrivo l'update e lo attacco(append) dopo la tabella
     if (json.update) {
         let p = document.createElement("p");
@@ -105,15 +143,26 @@ function visualizza (data, n) {
 }
 
 
+//Come dovro'modificare la funzione seguente?
+// 1)non sara piu' window.onload, ma sara' onsubmit o onclick del bottone del form
+// 2)per gestire diverse chiamate dovro' sostituire i div che creo durante la chiamata precedente, per farlo ho tre idee:
+//      -replaceChild
+//      -removeChild e poi ricreo gli stessi div
+//      -oppure creo i div fissi gia'  nell'html e poi riempio e svuoto questi div in base all'autore scelto
 
 //in jQuery sarebbe: $(document).ready(function()); oppure in maniera piu' compatta:  $(function(){
 window.onload = function () {
     //Gli passo i numeri per capire dove deve andare a inserire i vari grafici
     //for(var i=0; i<5; ++i){
     loadASync("scatterPlot.json", visualizza, 1);
+    loadASync("wordCloud.json", visualizza, 2);
     //significa che come file deve prendere quello nella stessa cartella di 'funzioni.js' e che in caso di 'success'===true allora deve entrare in visualizza
-    loadASync("histogram1.json", visualizza, 2);
-    loadASync("bubblePlot.json", visualizza, 3);
-    loadASync("wordCloud.json", visualizza, 4);
+    loadASync("histogramLegendaInterattiva.json", visualizza, 3);
+    loadASync("bubblePlot.json", visualizza, 4);
+    loadASync("histogram2.json", visualizza, 5);
+    loadASync("bubblePlotRiviste.json", visualizza, 6);
+
+
+
     //}
 };
